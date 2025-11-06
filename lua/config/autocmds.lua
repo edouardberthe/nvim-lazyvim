4ddc177a9f14c136f247cd4f1f0050f27b0ed975
Edouard Berthe <edouard.berthe@protonmail.com>
2025-11-06 11:25:28 +0000

    Add cutlass and other custom plugins

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,14 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  pattern = "*",
+  group = vim.api.nvim_create_augroup("NeotreeOnOpen", { clear = true }),
+  once = true,
+  callback = function(_)
+    if vim.fn.argc() == 0 then
+      vim.cmd("Neotree")
+    end
+  end,
+})
---
+++ lua/plugins/cutlass.lua
@@ -0,0 +1,12 @@
+return {
+  "gbprod/cutlass.nvim",
+  opts = {
+    cut_key = "x",
+    override_del = true,
+    registers = {
+      select = "s",
+      delete = "d",
+      change = "c",
+    },
+  },
+}
